2f8ae80971b917ca10d99999886d6399ddec87ed
Visal <visal.saosuo@gmail.com>
2022-09-03 12:52:47 +0700

    added basic datastructure

--- bot.js
+++ bot.js
@@ -7,7 +7,16 @@ token = JSON.parse(token.toString());
 const Telegraf = require('telegraf');
 const bot = new Telegraf(token.token);
 
+const users = new Map([ ["visal_saosuo", {confirmed: false, fname:"Visal", lname:"Saosuo", age:20, location:"Kampong Chhnang"}], 
+                        ["samnang_nounsinoeun", {confirmed: false, fname:"Samnang", lname:"Nounsinoeun", age:20, location:"Takeo"}]]);
+
+bot.use((ctx) =>{
+    console.log(users.get("@visalSaosuo"));
+    console.log(ctx.chat);
+})
+
 bot.start((ctx) => {
+    if(users.contain)
     ctx.reply("Welcome to D-Database");
 });
 
