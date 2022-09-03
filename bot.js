c38cae4d4fe6be7e927edc5483b57766adcab18d
Visal <visal.saosuo@gmail.com>
2022-09-03 14:08:56 +0700

    add confirmation

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const {readFileSync, promises: fsPromises} = require('fs');
+const { nextTick } = require('process');
 
 
 let token = readFileSync('TOKEN.json', 'utf-8');
@@ -7,18 +8,51 @@ token = JSON.parse(token.toString());
 const Telegraf = require('telegraf');
 const bot = new Telegraf(token.token);
 
-const users = new Map([ ["visal_saosuo", {confirmed: false, fname:"Visal", lname:"Saosuo", age:20, location:"Kampong Chhnang"}], 
-                        ["samnang_nounsinoeun", {confirmed: false, fname:"Samnang", lname:"Nounsinoeun", age:20, location:"Takeo"}]]);
+const users = new Map([ ["visal_saosuo", {confirmed: 0, fname:"វិសាល", lname:"សៅសួរ", age:20, location:"ខេត្តកំពុង់ឆ្នាំង"}], 
+                        ["samnang_nounsinoeun", {confirmed: 0, fname:"Samnang", lname:"Nounsinoeun", age:20, location:"Takeo"}]]);
 
-bot.use((ctx) =>{
-    console.log(users.get("@visalSaosuo"));
-    console.log(ctx.chat);
-})
+// bot.use((ctx) =>{
+//     console.log(users.get("@visalSaosuo"));
+//     console.log(ctx.chat);
+//     next(ctx);
+// })
 
 bot.start((ctx) => {
-    if(users.contain)
-    ctx.reply("Welcome to D-Database");
+    ctx.reply("ស្វាគមន៍មកកាន់ តារារះ​");
+    if(users.has(ctx.chat.username) && users.get(ctx.chat.username).confirmed == 0){
+        var user = users.get(ctx.chat.username);
+        var stringPrep = "សូមបញ្ជាក់ព័ត៌មានខាងក្រោម: " + 
+                        "\n\t\t\ឈ្មោះដើម: " + user.fname + 
+                        "\n\t\t\tនាមត្រកូល: " + user.lname +
+                        "\n\t\t\tអាយុ: " + user.age + 
+                        "\n\t\t\tទីតាំង: " + user.location
+        ctx.reply(stringPrep, {
+            reply_markup:{
+                inline_keyboard:[
+                    [{text:"ត្រឹមត្រូវ", callback_data:"userInfoConfirm"}],
+                    [{text:"មិនត្រឹមត្រូវ", callback_data:"userInforNotConfirm"}]
+                ]
+            }
+        });
+    }
 });
+
+bot.action("userInfoConfirm", ctx => {
+
+    if(users.has(ctx.chat.username))
+        users.get(ctx.chat.username).confirmed = 1;
+
+    ctx.answerCbQuery("ទទួលបាន");
+})
+
+bot.action("userInforNotConfirm", (ctx) => {
+
+    if(users.has(ctx.chat.username))
+        users.get(ctx.chat.username).confirmed = -1;
+
+    ctx.answerCbQuery("ទទួលបាន");
+
+})
 
 
 bot.launch();
